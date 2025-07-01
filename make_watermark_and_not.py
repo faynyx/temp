0a41b0b4c25beaee05772437faf129f7d3b990ae
faynyx <faynyx30@gmail.com>
2025-07-01 12:41:18 +0900

    Create make_watermark_and_not.py

---
+++ make_watermark_and_not.py
@@ -0,0 +1,38 @@
+import vertexai
+from vertexai.vision_models import ImageGenerationModel
+
+def generate_image(prompt: str):
+    
+    vertexai.init(project="project_name", location="us-central1")
+    generation_model = ImageGenerationModel.from_pretrained("imagen-4.0-generate-preview-06-06")
+    images = generation_model.generate_images(
+        prompt=prompt,
+        number_of_images=1,  
+        aspect_ratio="1:1",
+        add_watermark=True,
+    )
+    return images
+
+def generate_image_not_watermarking(prompt: str):
+    
+    vertexai.init(project="project_name", location="us-central1")
+    generation_model = ImageGenerationModel.from_pretrained("imagen-4.0-generate-preview-06-06")
+    images = generation_model.generate_images(
+        prompt=prompt,
+        number_of_images=1,  
+        aspect_ratio="1:1",
+        add_watermark=False,
+    )
+    return images
+
+if __name__ == "__main__":
+    prompt = "A cheerful little boy holding a giant sunflower, standing in a bright summer field, whimsical fairytale style"
+    try:
+        images = generate_image(prompt)
+        not_watermarking_image = generate_image_not_watermarking(prompt)
+
+        images[0].save("watermark_image.png")
+        not_watermarking_image[0].save("not_watermarking_image.png")
+
+    except Exception as e:
+        print ("Exception : ", e)
